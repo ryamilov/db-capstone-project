8a8a699cb7398a0b71ac1c1a4b6aab3f9e64aa64
Roman Yamilov <Roman.Yamilov@travelport.com>
2025-03-18 11:39:35 +0100

    more tasks of module 2 finished

--- tasksModule2.sql
+++ tasksModule2.sql
@@ -70,3 +70,133 @@ END//
 DELIMITER ; 
 
 call CancelOrder(5);
+
+-- Table booking system
+
+-- Task 1:
+INSERT INTO `LittleLemonDM`.`Bookings` (`BookingId`, `BookingDate`, `TableNumber`, `CustomerId`, `StaffId`, `MenuId`) VALUES
+(1, '2023-10-01', 5, 1, 1, 1),
+(2, '2023-10-02', 3, 2, 2, 2),
+(3, '2023-10-03', 7, 3, 3, 3),
+(4, '2023-10-04', 2, 4, 4, 4),
+(5, '2023-10-05', 8, 5, 5, 5),
+(6, '2023-10-06', 1, 6, 6, 6),
+(7, '2023-10-07', 4, 7, 7, 7),
+(8, '2023-10-08', 6, 8, 8, 8),
+(9, '2023-10-09', 9, 9, 9, 9),
+(10, '2023-10-10', 10, 10, 10, 10);
+
+-- Task 2:
+DELIMITER // 
+CREATE Procedure CheckBooking(bookingDateParam date, tableNumberParam int)
+Begin
+	SELECT CONCAT('Table ', TableNumber, ' already booked') 
+	FROM littlelemondm.bookings 
+    where BookingDate = bookingDateParam and TableNumber = tableNumberParam;
+END//
+DELIMITER ;
+
+call CheckBooking('2023-10-07', 4);
+
+-- Task 3:
+DELIMITER // 
+CREATE Procedure AddValidBooking(bookingDateParam date, tableNumberParam int)
+Begin
+	DECLARE numberOfRows INT;
+	START TRANSACTION; 
+    
+		SET @lastId = (select MAX(BookingId) from littlelemondm.bookings);
+		INSERT INTO littlelemondm.bookings 
+			(`BookingId`, `BookingDate`, `TableNumber`)
+		VALUES
+			(@lastId + 1, bookingDateParam, tableNumberParam); 
+            
+		Select count(*) into numberOfRows
+        from (select BookingDate, TableNumber, count(*) as numberOfBookings
+				FROM littlelemondm.bookings 
+                where BookingDate = bookingDateParam and TableNumber = tableNumberParam
+				Group by BookingDate, TableNumber
+				Having numberOfBookings > 1) as temp;
+                
+        IF numberOfRows > 0 
+			THEN 
+				select concat('table ', tableNumberParam, ' already booked - booking cancelled');
+                rollback;
+			ELSE 
+				select concat('table ', tableNumberParam, ' booked successfully on date ', bookingDateParam);
+                commit;
+        END IF;
+    rollback;
+END//
+DELIMITER ;
+
+call AddValidBooking('2023-10-08', 4);
+
+-- manage bookings
+-- Task 1:
+DELIMITER // 
+CREATE Procedure AddBooking(bookingIdParam int,
+	customerIdParam int,
+    bookingDateParam date, 
+    tableNumberParam int)
+Begin
+START TRANSACTION; 
+	INSERT INTO littlelemondm.bookings 
+		(`BookingId`, `CustomerId`, `BookingDate`, `TableNumber`)
+	VALUES
+		(bookingIdParam, customerIdParam, bookingDateParam, tableNumberParam); 
+	IF ROW_COUNT() > 0 
+		THEN 
+			commit;
+			select 'New booking added';
+		ELSE 
+			rollback;
+			select 'Cannot add the booking';
+    END IF;
+END//
+DELIMITER ;
+
+call AddBooking(12, 1, '2023-10-08', 4);
+
+-- Task 2:
+DELIMITER // 
+CREATE Procedure UpdateBooking(bookingIdParam int, bookingDateParam date)
+Begin
+START TRANSACTION; 
+	update littlelemondm.bookings 
+    set BookingDate = bookingDateParam
+    where BookingId = bookingIdParam;
+    
+	IF ROW_COUNT() > 0 
+		THEN 
+			commit;
+			select 'Booking updated';
+		ELSE 
+			rollback;
+			select 'Cannot update the booking';
+    END IF;
+END//
+DELIMITER ;
+
+call UpdateBooking(12, '2023-10-09');
+
+-- Task 3
+DELIMITER // 
+CREATE Procedure CancelBooking(bookingIdParam int)
+Begin
+START TRANSACTION; 
+	delete from littlelemondm.bookings 
+    where BookingId = bookingIdParam;
+    
+	IF ROW_COUNT() > 0 
+		THEN 
+			commit;
+			select concat('Booking ', bookingIdParam, ' cancelled');
+		ELSE 
+			rollback;
+			select 'Cannot cancel the booking';
+    END IF;
+END//
+DELIMITER ;
+
+call CancelBooking(12);
